4fad1c6bf71248e81a78fd1b9d9b1dede4be1d7a
pmundra <parag.mundra.ext@nokia.com>
2021-11-24 23:20:53 +0530

    frontend code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {message: ''};
+        this.state.currentDateAndTime = new Date().toLocaleString();
+    }
+
+    componentDidMount() {
+        const url = 'http://localhost:8080/sayhello?name=Stakater';
+        axios.get(url).then(response => {
+            console.log('response : ');
+            console.log(response);
+            this.setState({message: response.data});
+        }).catch(error => {
+            console.log('error : ');
+            console.log(error);
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.currentDateAndTime} + {this.state.message}
+            </div>
+        );
+    }
+}
+
+export default App;
